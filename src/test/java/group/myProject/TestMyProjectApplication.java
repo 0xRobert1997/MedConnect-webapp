408c71d0b9fc5ddb86ba6339069eb7d96a40c1d1
0xRobert <flaksik123@gmail.com>
2023-07-14 19:18:44 +0200

    adding some files and directories xd

---
+++ src/test/java/group/myProject/TestMyProjectApplication.java
@@ -0,0 +1,22 @@
+package group.myProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.context.annotation.Bean;
+import org.testcontainers.containers.PostgreSQLContainer;
+
+@TestConfiguration(proxyBeanMethods = false)
+public class TestMyProjectApplication {
+
+	@Bean
+	@ServiceConnection
+	PostgreSQLContainer<?> postgresContainer() {
+		return new PostgreSQLContainer<>("postgres:latest");
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.from(MyProjectApplication::main).with(TestMyProjectApplication.class).run(args);
+	}
+
+}
